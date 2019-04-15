2be631fca549274e36c65aa7ca1dbbfaa79b4fef
skyInGitHub <skymyuanti@gmail.com>
2019-04-16 07:53:34 +1000

    Run Kaldi s5 baseline of TIMIT dataset with run.sh and run_dnn.sh. Add 'data', 'data-fmllr-tri3', 'exp' and 'mfcc' folders.

--- egs/timit/s5/cmd.sh
+++ egs/timit/s5/cmd.sh
@@ -10,15 +10,21 @@
 # conf/queue.conf in http://kaldi-asr.org/doc/queue.html for more information,
 # or search for the string 'default_config' in utils/queue.pl or utils/slurm.pl.
 
-export train_cmd="queue.pl --mem 4G"
-export decode_cmd="queue.pl --mem 4G"
-# the use of cuda_cmd is deprecated, used only in 'nnet1',
-export cuda_cmd="queue.pl --gpu 1"
+#@sky
+#export train_cmd="queue.pl --mem 4G"
+#export decode_cmd="queue.pl --mem 4G"
+## the use of cuda_cmd is deprecated, used only in 'nnet1',
+#export cuda_cmd="queue.pl --gpu 1"
 
-if [[ "$(hostname -f)" == "*.fit.vutbr.cz" ]]; then
-  queue_conf=$HOME/queue_conf/default.conf # see example /homes/kazi/iveselyk/queue_conf/default.conf,
-  export train_cmd="queue.pl --config $queue_conf --mem 2G --matylda 0.2"
-  export decode_cmd="queue.pl --config $queue_conf --mem 3G --matylda 0.1"
-  export cuda_cmd="queue.pl --config $queue_conf --gpu 1 --mem 10G --tmp 40G"
-fi
+#if [[ "$(hostname -f)" == "*.fit.vutbr.cz" ]]; then
+#  queue_conf=$HOME/queue_conf/default.conf # see example /homes/kazi/iveselyk/queue_conf/default.conf,
+#  export train_cmd="queue.pl --config $queue_conf --mem 2G --matylda 0.2"
+#  export decode_cmd="queue.pl --config $queue_conf --mem 3G --matylda 0.1"
+#  export cuda_cmd="queue.pl --config $queue_conf --gpu 1 --mem 10G --tmp 40G"
+#fi
 
+#c) run locally... @sky
+export train_cmd="utils/run.pl"
+export decode_cmd="utils/run.pl"
+export cuda_cmd="utils/run.pl"
+export mkgraph_cmd="utils/run.pl"
